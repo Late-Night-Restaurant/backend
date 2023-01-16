10b050f553efd9f05401e97789124cc7b126de2c
weejinyoung <a8118199@gmail.com>
2023-01-17 03:49:56 +0900

    [ feat ] 현재 유저, 프로필로 리뷰 가져오기
    jpql 적용

--- src/main/java/com/backend/simya/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/backend/simya/domain/review/repository/ReviewRepository.java
@@ -3,13 +3,25 @@ package com.backend.simya.domain.review.repository;
 import com.backend.simya.domain.house.entity.House;
 import com.backend.simya.domain.profile.entity.Profile;
 import com.backend.simya.domain.review.entity.Review;
+import com.backend.simya.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-     List<Review> findReviewsByProfile(Profile profile);
+
+     @Query(value = "select review " +
+             "from Review review " +
+             "where review.profile.user.userId = :userId")
+     List<Review> findReviewsByUserId(Long userId);
+
+     @Query(value = "select review " +
+             "from Review review " +
+             "where review.profile.profileId = :profileId")
+     List<Review> findReviewsByProfileId(Long profileId);
+
      List<Review> findReviewsByHouse(House house);
 }
