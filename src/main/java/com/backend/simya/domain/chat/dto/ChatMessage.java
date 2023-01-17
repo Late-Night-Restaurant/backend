fcc9ae5f24d383d5d1ee88f44ca12380fa0d8480
jun02160 <jun020216@naver.com>
2023-01-17 20:47:33 +0900

    [ feat ] Chat DTO 구현

---
+++ src/main/java/com/backend/simya/domain/chat/dto/ChatMessage.java
@@ -0,0 +1,22 @@
+package com.backend.simya.domain.chat.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 채팅 메시지 타입 : 채팅방 입장(ENTER), 대화하기(TALK)
+ * - ENTER : 처음 채팅방에 들어오는 상태
+ * - TALK : 이미 세션에 연결되어 있고 채팅중인 상태
+ */
+@Getter @Setter
+public class ChatMessage {
+
+    public enum MessageType {
+        ENTER, TALK
+    }
+
+    private MessageType type;   // 메시지 유형
+    private String roomId;   // 방 번호
+    private String sender;   // 발신자
+    private String message;  // 메시지 데이터
+}
